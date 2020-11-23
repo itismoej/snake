21e8bed8d34f46182ffc7363d38fb8f705010cf1
Mohammad Jafari <m.jafari9877@gmail.com>
2020-11-23 23:10:04 +0330

    add snaking app

--- config/settings.py
+++ config/settings.py
@@ -37,6 +37,7 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'snaking',
     'channels',
 ]
 
@@ -119,3 +120,6 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/3.1/howto/static-files/
 
 STATIC_URL = '/static/'
+
+# Channels
+ASGI_APPLICATION = 'config.asgi.application'
---
+++ snaking/apps.py
@@ -0,0 +1,5 @@
+from django.apps import AppConfig
+
+
+class SnakingConfig(AppConfig):
+    name = 'snaking'
