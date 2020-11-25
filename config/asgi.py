002de344b8f6f9a3659a141cead7f2eb475d251a
Mohammad Jafari <m.jafari9877@gmail.com>
2020-11-25 15:49:51 +0330

    fix bug in asgi file

--- config/asgi.py
+++ config/asgi.py
@@ -6,20 +6,21 @@ It exposes the ASGI callable as a module-level variable named ``application``.
 For more information on this file, see
 https://docs.djangoproject.com/en/3.1/howto/deployment/asgi/
 """
-
 import os
+
+from django.core.asgi import get_asgi_application
+
+django_asgi_app = get_asgi_application()
 
 from channels.auth import AuthMiddlewareStack
 from channels.routing import ProtocolTypeRouter, URLRouter
-from django.core.asgi import get_asgi_application
-
 from snaking import routing
 
 os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'config.settings')
 
 application = ProtocolTypeRouter({
-  "http": get_asgi_application(),
-  "websocket": AuthMiddlewareStack(
+    "http": django_asgi_app,
+    "websocket": AuthMiddlewareStack(
         URLRouter(
             routing.websocket_urlpatterns
         )
